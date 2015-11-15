af600d1d3535fd57268c84fa1811ed54d49aedef
Vorquel <vorquel@gmail.com>
2015-11-14 17:56:23 -0700

    Finish main content

--- src/main/java/vorquel/mod/quickstack/ClientProxy.java
+++ src/main/java/vorquel/mod/quickstack/ClientProxy.java
@@ -1,8 +1,12 @@
 package vorquel.mod.quickstack;
 
 
+import net.minecraft.client.settings.KeyBinding;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.FMLCommonHandler;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import org.lwjgl.input.Keyboard;
 
 public class ClientProxy extends Proxy {
     
@@ -16,5 +20,17 @@ public class ClientProxy extends Proxy {
     @Override
     public void init() {
         FMLCommonHandler.instance().bus().register(handler);
+    }
+    
+    public static class QuickStackHandler {
+    
+        public KeyBinding keyBinding = new KeyBinding("Quick Stack", Keyboard.KEY_X, "Quick Stack");
+    
+        @SubscribeEvent
+        public void quickStack(InputEvent.KeyInputEvent event) {
+            if(!keyBinding.isPressed())
+                return;
+            QuickStack.network.sendToServer(MessageQuickStack.instance);
+        }
     }
 }
--- src/main/java/vorquel/mod/quickstack/QuickStack.java
+++ src/main/java/vorquel/mod/quickstack/QuickStack.java
@@ -4,6 +4,9 @@ import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.common.network.NetworkRegistry;
+import net.minecraftforge.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import net.minecraftforge.fml.relauncher.Side;
 
 @Mod(modid = "QuickStack", name = "Quick Stack", useMetadata = true)
 public class QuickStack {
@@ -14,9 +17,13 @@ public class QuickStack {
     @SidedProxy(serverSide = "vorquel.mod.quickstack.Proxy", clientSide = "vorquel.mod.quickstack.ClientProxy")
     public static Proxy proxy;
     
+    public static SimpleNetworkWrapper network;
+    
     @Mod.EventHandler
     public void preInit(FMLPreInitializationEvent event) {
         proxy.preInit();
+        network = NetworkRegistry.INSTANCE.newSimpleChannel("QuickStack");
+        network.registerMessage(MessageQuickStack.Handler.class, MessageQuickStack.class, 0, Side.SERVER);
     }
     
     @Mod.EventHandler
